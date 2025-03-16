b74b73febde57ff6ddae4c00d0cdf5ff9e0b45c6
Aaron Maryfield <aawemaryfield@gmail.com>
2025-03-16 05:59:40 -0700

    Initial commit

---
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -0,0 +1,53 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace HelloWorld
+{
+public class HelloWorldPlayer : NetworkBehaviour
+{
+public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+
+public override void OnNetworkSpawn()
+{
+Position.OnValueChanged += OnStateChanged;
+
+if (IsOwner)
+{
+Move();
+}
+}
+
+public override void OnNetworkDespawn()
+{
+Position.OnValueChanged -= OnStateChanged;
+}
+
+public void OnStateChanged(Vector3 previous, Vector3 current)
+{
+// note: `Position.Value` will be equal to `current` here
+if (Position.Value != previous)
+{
+transform.position = Position.Value;
+}
+}
+// When local player spawns or Move button in UI is pressed
+// RPC call tells server to assign a new position
+public void Move()
+{
+SubmitPositionRequestServerRpc();
+}
+
+[Rpc(SendTo.Server)]
+void SubmitPositionRequestServerRpc(RpcParams rpcParams = default)
+{
+var randomPosition = GetRandomPositionOnPlane();
+transform.position = randomPosition;
+Position.Value = randomPosition;
+}
+
+static Vector3 GetRandomPositionOnPlane()
+{
+return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+}
+}
+}
